433d7590b69b4de0f836d4ebdfb2a6ff2cd52f8f
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-02-08 19:34:04 +0100

    Moves the code for executing the current function to the execute_command function to improve code readability

--- gondolier.py
+++ gondolier.py
@@ -1,21 +1,29 @@
-import tkinter as tk
 import subprocess
+import tkinter as tk
 
-# Define the terminal command to execute
-command = "docker ps -a"
 
-# Execute the command
-result = subprocess.run(command, shell=True, capture_output=True, text=True)
+def execute_command(current_command="docker ps -a"):
+    # Define the terminal command to execute
+    command = current_command
 
-# Check the result
-if result.returncode == 0:
-    print("Command executed successfully.")
-    print("Output:")
-    print(result.stdout)
-else:
-    print("Error executing command.")
-    print("Error message:")
-    print(result.stderr)
+    # Execute the command
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    # Check the result
+    if result.returncode == 0:
+        print("Command executed successfully.")
+        print("Output:")
+        print(result.stdout)
+
+        # Parse the output into rows
+        rows = [line.split() for line in result.stdout.strip().split("\n")]
+    else:
+        print("Error executing command.")
+        print("Error message:")
+        print(result.stderr)
+
+
+execute_command()
 
 # Create a Tkinter window
 window = tk.Tk()
